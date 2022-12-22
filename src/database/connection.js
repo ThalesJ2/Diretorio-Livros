03b128ef5e1ba15f6e4b5cb0f73e5bef6a8d6187
Thales Jorge <thalesjorge36@gmail.com>
2022-12-22 02:53:43 -0300

    .

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({path: 'C:\\Users\\thale\\Desktop\\Diretorio-Livro\\.env'});
 const knex = require('knex')({
     client: 'mysql2',
     connection: {
