db3ce5a8fe63db8e926c0e245422a7457e8db4d3
Thales Jorge <thalesjorge36@gmail.com>
2022-12-31 19:29:09 -0300

    .

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,11 +4,11 @@ console.log(process.env.DB_PASSWORD);
 const knex = require('knex')({
     client: 'mysql2',
     connection: {
-      host : process.env.DB_HOST,
-      port : process.env.DB_PORT,
-      user : process.env.DB_USER,
-      password:process.env.DB_PASSWORD,
-      database : process.env.DB_NAME
+      host : 'localhost',
+      port : '3306',
+      user : 'root',
+      password:'123456789',
+      database : 'diretorio'
     }
   });
 
