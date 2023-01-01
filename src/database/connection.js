83d0f8a237dc7fe0907a58dc905a749bc7de33a3
Thales Jorge <thalesjorge36@gmail.com>
2022-12-31 22:25:47 -0300

    .

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,14 +1,13 @@
 const path = require("path");
 require("dotenv").config();
-console.log(process.env.DB_PASSWORD);
 const knex = require('knex')({
     client: 'mysql2',
     connection: {
-      host : 'localhost',
-      port : '3306',
-      user : 'root',
-      password:'123456789',
-      database : 'diretorio'
+      host : process.env.DB_HOST,
+      port : process.env.DB_PORT,
+      user : process.env.DB_USER,
+      password:process.env.DB_PASSWORD,
+      database : process.env.DB_NAME
     }
   });
 
