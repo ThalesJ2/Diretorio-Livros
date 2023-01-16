7f8f381c06a026f61c21673ebf6943ed40e9fafd
Thales Jorge <thalesjorge36@gmail.com>
2023-01-16 11:36:37 -0300

    .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyparser = require("body-parser");
 const routes = require("./routes/routes");
 const log = require("./Logs/log");
-const PORT  =  process.env.DB_port || 4545;
+const PORT  =  process.env.DB_test || 4545;
 app.use(log);
 
 app.use(bodyparser.urlencoded({extended:false}));
@@ -12,5 +12,5 @@ app.use(routes);
 
 
 app.listen(PORT,()=>{
-    console.log("app rodandos!");
+    console.log("app"+PORT);
 });
