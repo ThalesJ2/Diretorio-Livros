16975b1bd685220a8ee81bbed3653b5f6b9aac7b
Thales Jorge <thalesjorge36@gmail.com>
2023-01-15 21:09:49 -0300

    .

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 const knex = require('knex')({
     client: 'mysql2',
     connection: {
-      socketPath: '/cloudsql/charbot2-ivjagp:asia-south1:auction-instance',
-      port : process.env.DB_PORT,
+      host:process.env.DB_HOST,
+      port :process.env.DB_PORT,
       user : process.env.DB_USER,
       password:process.env.DB_PASSWORD,
       database : process.env.DB_NAME
